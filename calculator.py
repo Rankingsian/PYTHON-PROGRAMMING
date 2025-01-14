5771640623171cb26fbd098b512d8eddc8199832
Mwangi Ian <140388111+Rankingsian@users.noreply.github.com>
2025-01-14 23:25:42 +0300

    commit m

---
+++ calculator.py
@@ -0,0 +1,19 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+sign = input("Enter the sign (+, -, *, /): ")
+if sign == "+":
+    result = number1 + number2
+    print(result)
+elif sign =="-":
+    result = number1 - number2
+    print (result)
+elif sign =="*":
+    result = number1 * number2
+    print(result)
+elif sign == "/":
+    result = number1 / number2
+    print(result)
+elif sign == ""
+    
+else :
+    print("Invalid sign")
